f017c2b0e8c501bafbc3f10f503c2f5331ad3076
Adam Morris <116088370+AdamMorris668@users.noreply.github.com>
2023-02-02 14:21:08 +0000

    Create footer.php

---
+++ SRC/footer.php
@@ -0,0 +1,13 @@
+<!-- footer to be used on all customer pages -->
+<!-- make sure w3 school style sheet is imported -->
+<footer class="w3-container w3-padding-64 w3-center w3-light-grey w3-xlarge">
+  <p>Follow us on social media</p>
+  <i class="fa fa-facebook-official w3-hover-opacity"></i>
+  <i class="fa fa-instagram w3-hover-opacity"></i>
+  <i class="fa fa-snapchat w3-hover-opacity"></i>
+  <i class="fa fa-pinterest-p w3-hover-opacity"></i>
+  <i class="fa fa-twitter w3-hover-opacity"></i>
+  <i class="fa fa-linkedin w3-hover-opacity"></i>
+  <p class="w3-smal"></p> 2022-2023</p>
+  <p class="w3-medium">Designed By: <a href="index.php?page=home" target="_blank">Team Number 16</a></p>
+</footer>
