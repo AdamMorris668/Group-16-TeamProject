a8f1a5f2fc6cd24fda9c2a6b332f50544ff0a0eb
Rayan Abdulsmad <210060906@aston.ac.uk>
2023-02-23 14:46:02 +0000

    Admin Login

--- adminLoginJava.java
+++ adminLoginJava.java
@@ -5,9 +5,7 @@ pass = txtPass.getText();
 
 if(pass.equals("password") && user.equals("username") ){
     lblDisplay.setText("Credentials Accepted.");
-    // Redirect the user to index.html
     try {
-        // Replace "index.html" with the desired URL to redirect the user to
         URI uri = new URI("admin.html");
         java.awt.Desktop.getDesktop().browse(uri);
     } catch (Exception e) {
