699ffd77644b1dd1e41008b1e8a04407e0af48c5
210136485 <104042668+210136485@users.noreply.github.com>
2023-03-22 18:06:29 +0000

    Update techtopiadatabase.sql

--- SRC/sql/techtopiadatabase.sql
+++ SRC/sql/techtopiadatabase.sql
@@ -70,8 +70,16 @@ CREATE TABLE `products` (
 --
 
 INSERT INTO `products` (`id`, `product`, `product_description`, `quantity`, `price`, `type`, `images`) VALUES
-(1, 'LCD Monitor', '24” Black LCD Monitor with silver base.', 200, '80.00', 'monitor', '../Images/product-img/A1.jpg'),
-(2, 'LED Monitor', '30” Full Black LED Display Monitor.', 250, '100.00', 'monitor', '../Images/product-img/A2.jpg'),
+(1, 'LCD Monitor', '24” Black LCD Monitor with silver base, Refresh Rate. Resolution. 1920 x 1080.
+Panel Type: IPS. Aspect Ratio. 16:9.
+Display Resolution: FHD. Brightness. 250(Typ), 200(min) cd/m2.
+Color Gamut: 72% Response Time. 5ms (Faster)
+Contrast Ratio (DFC) 1000:1 (Typ.) Viewing Angle. 178 / 178.
+Surface Treatment. Anti glare ,3H.', 200, '80.00', 'monitor', '../Images/product-img/A1.jpg'),
+(2, 'LED Monitor', '30” Full Black LED Display Monitor. IPS Display
+Sleek Cut Design
+Black Stabilizer
+Reader Mode/Flicker Safe', 250, '100.00', 'monitor', '../Images/product-img/A2.jpg'),
 (3, 'Curved Monitor', '32” Full Black, Curved Ultrawide Display Monitor.', 1, '150.00', 'monitor', '../Images/product-img/A3.jpg'),
 (4, 'Touch Screen Monitor', '24” Full Black Matte, Smooth touch screen monitor.', 50, '180.00', 'monitor', '../Images/product-img/A4.jpg'),
 (5, 'Large Wall Monitor', '84” Full Black, Edge LED, large screen display.', 250, '220.00', 'monitor', '../Images/product-img/A5.jpg'),
