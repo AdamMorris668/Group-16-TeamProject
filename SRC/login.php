1a7f42800a3eb0754c5aa9ca4cb11e5ea65ccd6d
Rayan Abdulsmad <210060906@aston.ac.uk>
2023-03-25 21:47:13 +0000

    Footer/Header
    Header and Footer added

--- SRC/login.php
+++ SRC/login.php
@@ -13,6 +13,7 @@ if(isset($_SESSION["user_id"])) {
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="formsstyle.css">
+    <?php include 'header.php'; ?>
     <title>My Account - Login</title>
 
   </head>
@@ -55,4 +56,5 @@ if(isset($_SESSION["user_id"])) {
     </div>
   
 </div>
+<?php include 'footer.php'; ?>
 <body> 
--- SRC/register.php
+++ SRC/register.php
@@ -46,7 +46,8 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="formsstyle.css">
+    <?php include 'header.php'; ?>
 </head>
 
 <body>
@@ -102,4 +103,6 @@
 
 </form>
     <script src="script.js"></script>
+    <?php include 'footer.php'; ?>
+
 </body>
