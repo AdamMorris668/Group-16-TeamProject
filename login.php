dee9a461ddc13c0a059c75229d7ca7cc80e1ec57
Rayan Abdulsmad <210060906@aston.ac.uk>
2023-03-25 19:30:55 +0000

    Update login.php

--- login.php
+++ login.php
@@ -12,7 +12,7 @@ if(isset($_SESSION["user_id"])) {
 <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="formsstyle.css">
     <title>My Account - Login</title>
 
   </head>
