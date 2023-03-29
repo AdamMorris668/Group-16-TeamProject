f2dca79722727a276a39c3e2417f20a17506b3a1
Jawraj21 <210160435@aston.ac.uk>
2023-03-29 23:29:18 +0100

    Database updates

--- SRC/sql/techtopiadatabase.sql
+++ SRC/sql/techtopiadatabase.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Mar 29, 2023 at 02:34 AM
+-- Generation Time: Mar 30, 2023 at 12:18 AM
 -- Server version: 10.4.22-MariaDB
 -- PHP Version: 8.1.2
 
@@ -58,8 +58,16 @@ INSERT INTO `admin_login` (`id`, `email`, `username`, `password`) VALUES
 CREATE TABLE `basket` (
   `id` int(11) NOT NULL,
   `user_id` int(11) NOT NULL,
-  `product_id` int(11) NOT NULL
+  `product_id` int(11) NOT NULL,
+  `quantity` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_520_ci;
+
+--
+-- Dumping data for table `basket`
+--
+
+INSERT INTO `basket` (`id`, `user_id`, `product_id`, `quantity`) VALUES
+(56, 10, 19, 1);
 
 -- --------------------------------------------------------
 
@@ -82,11 +90,6 @@ CREATE TABLE `contactus` (
 
 INSERT INTO `contactus` (`id`, `user_id`, `user_firstname`, `user_subject`, `user_message`, `message_date`) VALUES
 (9, 10, 'alex', 'alex', 'alex', '2023-03-27'),
-(10, 10, 'alex', 'alex', 'alex', '2023-03-27'),
-(11, 10, 'alex', 'alex', 'alex', '2023-03-27'),
-(12, 10, 'alex', 'alex', 'alex', '2023-03-27'),
-(13, 10, 'alex', 'alex', 'alex', '2023-03-27'),
-(14, 10, 'alex', 'alex', 'alex', '2023-03-27'),
 (15, 10, 'alex', 'alex', 'alex', '2023-03-27');
 
 -- --------------------------------------------------------
@@ -99,6 +102,7 @@ CREATE TABLE `orders` (
   `id` int(11) NOT NULL,
   `user_id` int(11) NOT NULL,
   `product_id` int(11) NOT NULL,
+  `quantity` int(11) NOT NULL,
   `order_status` varchar(50) COLLATE utf8mb4_unicode_520_ci NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_520_ci;
 
@@ -106,10 +110,10 @@ CREATE TABLE `orders` (
 -- Dumping data for table `orders`
 --
 
-INSERT INTO `orders` (`id`, `user_id`, `product_id`, `order_status`) VALUES
-(1, 10, 2, 'cancelled'),
-(2, 10, 2, 'processing'),
-(4, 10, 6, 'delivered');
+INSERT INTO `orders` (`id`, `user_id`, `product_id`, `quantity`, `order_status`) VALUES
+(1, 10, 2, 1, 'cancelled'),
+(2, 10, 2, 1, 'processing'),
+(4, 10, 6, 3,'delivered');
 
 -- --------------------------------------------------------
 
@@ -132,12 +136,12 @@ CREATE TABLE `products` (
 --
 
 INSERT INTO `products` (`id`, `product`, `product_description`, `size`, `price`, `type`, `images`) VALUES
-(1, 'LCD Monitor', '24” Black LCD Monitor with silver base.', 200, '80.00', 'monitor', 'images/monitors/A1.jpg'),
-(2, 'LED Monitor', '30” Full Black LED Display Monitor.', 250, '100.00', 'monitor', 'images/monitors/A2.jpg'),
+(1, 'LCD Monitor', '24” Black LCD Monitor with silver base.', 196, '80.00', 'monitor', 'images/monitors/A1.jpg'),
+(2, 'LED Monitor', '30” Full Black LED Display Monitor.', 247, '100.00', 'monitor', 'images/monitors/A2.jpg'),
 (3, 'Curved Monitor', '32” Full Black, Curved Ultrawide Display Monitor.', 1, '150.00', 'monitor', 'images/monitors/A3.jpg'),
 (4, 'Touch Screen Monitor', '24” Full Black Matte, Smooth touch screen monitor.', 50, '180.00', 'monitor', 'images/monitors/A4.jpg'),
 (5, 'Large Wall Monitor', '84” Full Black, Edge LED, large screen display.', 250, '220.00', 'monitor', 'images/monitors/A5.jpg'),
-(6, 'Wired Keyboard ', 'Simple matte black wired desktop keyboard.', 200, '40.00', 'keyboard', 'images/keyboards/B1.jpg'),
+(6, 'Wired Keyboard ', 'Simple matte black wired desktop keyboard.', 195, '40.00', 'keyboard', 'images/keyboards/B1.jpg'),
 (7, 'Wireless Keyboard ', 'Slim full white wireless desktop keyboard.', 250, '50.00', 'keyboard', 'images/keyboards/B2.jpg'),
 (8, 'Gaming Keyboard ', 'Black wireless mechanical gaming keyboard, with RGB lighting.', 1, '70.00', 'keyboard', 'images/keyboards/B3.jpg'),
 (9, 'Laser Sensor Keyboard ', 'Portable and rechargeable laser sensor keyboard.', 50, '120.00', 'keyboard', 'images/keyboards/B4.jpg'),
@@ -151,7 +155,7 @@ INSERT INTO `products` (`id`, `product`, `product_description`, `size`, `price`,
 (17, 'Compact Camera    ', 'Black, pocket compact camera 42 mega pixel. ', 250, '120.00', 'camera', 'images/cameras/D2.jpg'),
 (18, 'Camera 360  ', 'Black, portable 360° angle camera.', 1, '300.00', 'camera', 'images/cameras/D3.jpg'),
 (19, 'Web Camera  ', 'Black, wide angle desktop web camera..', 50, '50.00', 'camera', 'images/cameras/D4.jpg'),
-(20, 'Photo Print Camera ', 'White, portable photo print camera. .', 250, '250.00', 'camera', 'images/cameras/D5.jpg'),
+(20, 'Photo Print Camera ', 'White, portable photo print camera. .', 0, '250.00', 'camera', 'images/cameras/D5.jpg'),
 (21, 'Portable Bluetooth Speaker ', 'White/Black, metallic, rechargeable, portable Bluetooth speaker.', 200, '70.00', 'speaker', 'images/speakers/E1.jpg'),
 (22, 'Woofer Speaker   ', 'Black, standing woofer with four-sided voice system.', 250, '120.00', 'speaker', 'images/speakers/E2.jpg'),
 (23, 'Bar speaker  ', 'Black, Metallic framed bar speaker.', 1, '80.00', 'speaker', 'images/speakers/E3.jpg'),
@@ -183,7 +187,6 @@ INSERT INTO `user` (`id`, `username`, `email`, `password`, `user_firstname`, `us
 (1, 'jawraj', 'jawraj@gmail.com', '$2y$10$rzAs/6IUjRYH/TZXyJYsf.kj251JRZadMba1TAbGXYP2Xpql5Zeja', 'Jawraj', 'Singh', '21 GT road', '2023-03-26'),
 (2, 'mark', 'mark@gmail.com', '$2y$10$rAWfe9kF/DXssgzcqkHe0Of.tftCW3XOQ.gzFJoz.9wisyCmwiLpG', 'Mark', 'Mark', '21 GT road', '2023-03-26'),
 (6, 'stefano', 'stefano@gmail.com', '$2y$10$HJwWNTI.cVjtHk00uOhFY.jSrT21krROGjM9Y8u5eXTxJ33LgDGny', 'stefano', 'stefano', '24 stefano', '2023-03-26'),
-(9, 'alex', 'alex@gmail.com', '$2y$10$5CDZM0IbZVkuF5NLWrHu.e7DZhL0lL2Vq/An0yhf9WnJkJiFRfL2u', 'alex', 'alex', '21 alex', '2023-03-27'),
 (10, 'alex', 'alex@gmail.com', '$2y$10$pQlN0Rz45ltkgQZULV5rHeX/xN5GK8woNTHC6CXtyBDtCiaDr5TvC', 'alex', 'alex', '1 alex', '2023-03-27');
 
 -- --------------------------------------------------------
@@ -258,7 +261,7 @@ ALTER TABLE `admin_login`
 -- AUTO_INCREMENT for table `basket`
 --
 ALTER TABLE `basket`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=21;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=57;
 
 --
 -- AUTO_INCREMENT for table `contactus`
@@ -270,7 +273,7 @@ ALTER TABLE `contactus`
 -- AUTO_INCREMENT for table `orders`
 --
 ALTER TABLE `orders`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
 -- AUTO_INCREMENT for table `products`
@@ -288,7 +291,7 @@ ALTER TABLE `user`
 -- AUTO_INCREMENT for table `wishlist`
 --
 ALTER TABLE `wishlist`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
